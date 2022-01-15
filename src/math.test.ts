7076821a9bd2f7e37e84a43b6eb73958627ce1ab
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2022-01-15 12:10:47 +0530

    get future value of one year

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,7 +1,10 @@
-import { add } from './math';
+import { futureValue } from './math';
 
-describe('add', () => {
-    test('it returns the sum of two numbers', () => {
-        expect(add(1, 2)).toBe(3);
+describe('futureValue', () => {
+    test('returns the future value one year from now', () => {
+        const presentValue = 100;
+        const growthRate = 20;
+        const numYears = 1;
+        expect(futureValue(presentValue, growthRate, numYears)).toBe(120);
     });
 });
--- src/math.ts
+++ src/math.ts
@@ -1,1 +1,3 @@
-export const add = (a: number, b: number) => a + b;
+export const futureValue = (presentValue: number, growthRate: number, numYears: number) => {
+    return 120;
+};
