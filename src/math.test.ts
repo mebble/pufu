8f9b48e402ef1cb3f8f1ef86740b6a71f1c27312
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2022-01-15 12:32:41 +0530

    get future value when growth rate is negative

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,18 +1,37 @@
 import { futureValue } from './math';
 
 describe('futureValue', () => {
-    test('returns the future value one year from now', () => {
-        const presentValue = 100;
-        const growthRate = 20;
+    describe('positive growth rate', () => {
+        test('returns the future value one year from now', () => {
+            const presentValue = 100;
+            const growthRate = 20;
 
-        expect(futureValue(presentValue, growthRate, 1)).toBe(120);
+            expect(futureValue(presentValue, growthRate, 1)).toBe(120);
+        });
+
+        test('returns the future value rounded to the nearest 2 decimal places', () => {
+            const presentValue = 100;
+
+            expect(futureValue(presentValue, 20, 3)).toBe(172.8);
+            expect(futureValue(presentValue, 20, 4)).toBe(207.36);
+            expect(futureValue(presentValue, 23, 3)).toBe(186.09);
+        });
     });
 
-    test('returns the future value rounded to the nearest 2 decimal places', () => {
-        const presentValue = 100;
+    describe('negative growth rate', () => {
+        test('returns the future value one year from now', () => {
+            const presentValue = 100;
+            const growthRate = -20;
 
-        expect(futureValue(presentValue, 20, 3)).toBe(172.8);
-        expect(futureValue(presentValue, 20, 4)).toBe(207.36);
-        expect(futureValue(presentValue, 23, 3)).toBe(186.09);
+            expect(futureValue(presentValue, growthRate, 1)).toBe(80);
+        });
+
+        test('returns the future value rounded to the nearest 2 decimal places', () => {
+            const presentValue = 100;
+
+            expect(futureValue(presentValue, -20, 3)).toBe(51.2);
+            expect(futureValue(presentValue, -20, 4)).toBe(40.96);
+            expect(futureValue(presentValue, -24, 3)).toBe(43.9);
+        });
     });
 });
