fee4a007df442becd5da795dc091a0a8c055fa02
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2022-01-15 12:28:17 +0530

    get future value of X years

--- src/math.test.ts
+++ src/math.test.ts
@@ -4,7 +4,15 @@ describe('futureValue', () => {
     test('returns the future value one year from now', () => {
         const presentValue = 100;
         const growthRate = 20;
-        const numYears = 1;
-        expect(futureValue(presentValue, growthRate, numYears)).toBe(120);
+
+        expect(futureValue(presentValue, growthRate, 1)).toBe(120);
+    });
+
+    test('returns the future value rounded to the nearest 2 decimal places', () => {
+        const presentValue = 100;
+
+        expect(futureValue(presentValue, 20, 3)).toBe(172.8);
+        expect(futureValue(presentValue, 20, 4)).toBe(207.36);
+        expect(futureValue(presentValue, 23, 3)).toBe(186.09);
     });
 });
--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,6 @@
-export const futureValue = (presentValue: number, growthRate: number, numYears: number) => {
-    return 120;
+export const futureValue = (presentValue: number, growthPercentage: number, numYears: number) => {
+    const growthRate = growthPercentage / 100;
+    const result = presentValue * ((1 + growthRate) ** numYears);
+    const rounded = Math.round(result * 100) / 100;
+    return rounded;
 };
